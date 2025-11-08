c051aa142b79a7d0d2d26eadfd9fefc34ab61c01
PhoenixS1 <psmith25@pratt.edu>
2025-11-07 20:13:43 -0500

    Update geneflo.js

--- geneflo.js
+++ geneflo.js
@@ -2,6 +2,6 @@
  	var response = await fetch('geneflo.json');
 	var data = await response.json();
 	var imageUrl = data.url;
-	document.getElementById('Batesdemogrpahics').innerHTML = `<img src="${imageUrl}" alt="Flow Demo">`;
+	document.getElementById('Batesdemogrpahics').innerHTML = `<img src="https://informationr.net/ir/27-SpIssue/CoLIS2022/figs/colis2201fig1.png" alt="Flow Demo">`;
     }
 
